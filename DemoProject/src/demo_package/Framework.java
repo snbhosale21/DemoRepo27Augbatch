0879eff74168efdf0b5adcaaf44779f785f4343a
snbhosale21 <snbhosale2100@gmail.com>
2023-01-30 15:05:00 +0530

    BasicTask

---
+++ DemoProject/src/demo_package/Framework.java
@@ -0,0 +1,15 @@
+package demo_package;
+
+public class Framework {
+	
+	public static void main(String[] args) {
+		System.out.println("Eclipse");
+		System.out.println("Maven");
+		System.out.println("Selenium");
+		System.out.println("apache");
+		System.out.println("Github");
+		System.out.println("Extentreport");
+		
+	}
+
+}
